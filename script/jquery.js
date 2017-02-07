36e03ee5faa5cc8c76eebf9b980295c399fa379c
Jonathan Sebastian <jojosebseb@gmail.com>
2017-02-07 16:39:38 +0700

    v0.2

--- script/jquery.js
+++ script/jquery.js
@@ -2,3 +2,23 @@ $('.slider-container').slick({
     dots: true,
     arrows: false
 })
+$('.media-slider-container').slick({
+    dots: true,
+    arrows: false
+})
+
+var scrolled = 0;
+var selClass = "scrollAbout";
+
+function bgMove(selClass){
+    $('#'+selClass).css({
+        'background-position': 'center '+ -scrolled +'px'
+    })
+}
+
+
+
+$(window).on('scroll', function(){
+    scrolled = $(this).scrollTop()/5;
+    bgMove('scrollAbout');
+})
